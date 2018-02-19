2cbc5ec0eca04c36d0f8445671afd24bd9312660
Patrick Steranka <Patrick@Steranka.com>
2018-02-19 17:21:31 -0500

    Changes to support production build and deployment to github pages (gh-pages).

--- Webpack2/300 - BasicApp/webpack.config.js
+++ Webpack2/300 - BasicApp/webpack.config.js
@@ -73,6 +73,9 @@ var config = {
             names: ['vendor', 'manifest' ]
         })
         , new webpack.HotModuleReplacementPlugin()
+        , new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
     ],
 }
 
